d5a8b608f0d08d07400edf536b6ffd07e87b3fd3
MartinO55 <martinguyolson@gmail.com>
2022-09-05 20:27:34 +1000

    added new page for the actual story. added js file for functions that is now hilariously misnamed. worked on the some of the css animations

---
+++ index.js
@@ -0,0 +1,6 @@
+function clickThroughStory(destination) //so we want this function to fade out the text we are hovering over and fade in the next line, and once its gone, its gone
+{
+    document.querySelector(destination)
+    console.log("hi")
+}
+   
