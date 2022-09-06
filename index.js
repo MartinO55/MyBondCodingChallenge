d595fd841819a3947e7ced7be3ce3e6eefa6d4fc
MartinO55 <martinguyolson@gmail.com>
2022-09-06 11:50:57 +1000

    js function to show and hide paragraphs now works as you click on them, added style to hide line one two, added unfilled paragraph one three

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function clickThroughStory(destination) //so we want this function to fade out the text we are hovering over and fade in the next line, and once its gone, its gone
+function clickThroughStory(current,destination) //so we want this function to fade out the text we are hovering over and fade in the next line, and once its gone, its gone
 {
-    document.querySelector(destination)
-    console.log("hi")
+    targetToShow = document.querySelector(destination)//finds the target/next p in sequence as told by the html
+    targetToHide = document.querySelector(current)
+    console.log(targetToShow)//from here I guess we just alter the CSS show the target variable div and hide the previous one
+    targetToShow.style.display = 'block'
+    targetToHide.style.display = 'none'
+
 }
    
