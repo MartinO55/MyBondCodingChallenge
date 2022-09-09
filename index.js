91746179d0020a5a521d4061db00e782e2024745
MartinO55 <martinguyolson@gmail.com>
2022-09-09 12:31:07 +1000

    added comments to code for explanations :). added link to Github repo in index

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+//JS function to show/hide each line as needed. All the lines are already on the page, this just prevents the user being overwhelmed, and makes loading really smooth (cause there is no loading)
 function clickThroughStory(current,destination) //so we want this function to fade out the text we are hovering over and fade in the next line, and once its gone, its gone
 {
-    targetToShow = document.querySelector(destination)//finds the target/next p in sequence as told by the html
+    targetToShow = document.querySelector(destination)//finds the target/next p in sequence as told by the html from inferno
     targetToHide = document.querySelector(current)
-    //console.log(targetToShow)//from here I guess we just alter the CSS show the target variable div and hide the previous one
-    if (targetToShow != null) {
-        targetToShow.style.display = 'block' 
+ 
+    if (targetToShow != null) {//checks there is another line first
+        targetToShow.style.display = 'block' //just changes the CSS style 
         targetToHide.style.display = 'none'
-    } else{
+    } else{//goes back to the index page and leaves an error message
         window.location.href ="Index.html"
         console.log("error: either Virgil has not finished the tour or Dante has not finished writing")
     }
